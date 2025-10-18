6e22f5136874da04a43dc5a6f17c21e0a4f0fb8d
psh12320 <pshricharan2020@gmail.com>
2025-10-19 03:43:19 +0800

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -229,6 +229,6 @@ def retrieve_image(transaction_id):
     return jsonify({"source_path": txn.get("source_path"), "source_type": txn.get("source_type")})
 
 
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
